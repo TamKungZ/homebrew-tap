603a88ef30a40ae461405a677f86bc47144a4e9b
TamKungZ_ <kittiwut.pimpromma@gmail.com>
2026-04-14 03:59:09 +0700

    chore(cask): update imagemerge to 1.0.4 (#3)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/imagemerge.rb
+++ Casks/imagemerge.rb
@@ -1,14 +1,14 @@
 cask "imagemerge" do
-  version "1.0.3"
+  version "1.0.4"
 
   on_arm do
-    url "https://github.com/TamKungZ/ImageMergePy/releases/download/v1.0.3/ImageMerge-macos-binary-arm64.zip"
-    sha256 "74bda7bf649a1ae17230db90054b13dabe3081bdb6c7692bf57635536fc415b7"
+    url "https://github.com/TamKungZ/ImageMergePy/releases/download/v1.0.4/ImageMerge-macos-binary-arm64.zip"
+    sha256 "eb407b13414822cac973aface1af6304da40ec3b2b1b635643884ffe5283e3b3"
   end
 
   on_intel do
-    url "https://github.com/TamKungZ/ImageMergePy/releases/download/v1.0.3/ImageMerge-macos-binary-x64.zip"
-    sha256 "f22e385c15c100de52ed594f37b3de2e9b7ca0797a057ce09e24b23e532ec9a9"
+    url "https://github.com/TamKungZ/ImageMergePy/releases/download/v1.0.4/ImageMerge-macos-binary-x64.zip"
+    sha256 "c97115d6aa2008c0807e24d2c33eb8ad9980c8f52cb28c222bb8f9825ef5fa0c"
   end
 
   name "ImageMerge"
